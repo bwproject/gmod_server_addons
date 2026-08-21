f24e8d8a2fb1c76eab7f6d67bfe0e4f5da6a5bdb
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2026-08-21 10:16:35 +0500

    Fix ULib privilege group creation for User hierarchy

--- addons/bw_prop_hunts/lua/autorun/server/bw_prop_hunts_groups.lua
+++ addons/bw_prop_hunts/lua/autorun/server/bw_prop_hunts_groups.lua
@@ -5,7 +5,7 @@
 if not ULib or not ULib.ucl or not BW_PHX_GROUPS then return end
 
 for _, g in ipairs(BW_PHX_GROUPS) do
-    if not ULib.ucl.groups[g.id] then
+    if not ULib.ucl.groups[g.id] and g.id ~= "user" then
         local inherit = BW_PHX_GROUP_INHERITS[g.id]
         ULib.ucl.addGroup(g.id, {}, inherit and inherit[1] or "user")
     end
